940f4d7b8d6c0a1f7714bc42e009056a2b4aa978
KorkinIgor <igorkorkin55@gmail.com>
2022-12-21 16:27:51 +0600

    домашка

---
+++ Assets/Igrok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Igrok : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent _agent;
+    private Animator anim;
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+  
+    
+     
+    
+
+    void Update()
+    {
+        _agent.SetDestination(target.position);
+        anim.SetFloat("Speed",_agent.velocity.magnitude);
+    }
+}
